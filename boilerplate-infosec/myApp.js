0587bcce90259fce4445ea84010b36ac4dda8e43
trissentety <82165867+trissentety@users.noreply.github.com>
2023-11-02 21:07:14 -0700

    Update myApp.js

--- boilerplate-infosec/myApp.js
+++ boilerplate-infosec/myApp.js
@@ -1,23 +1,7 @@
-helmet in myApp.js.
-
 const express = require('express');
-const helmet = require('helmet');
 const app = express();
+const helmet = require('helmet');
 
-const PORT = process.env.PORT || 3030;
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api)
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App listening on port ${PORT}`);
-});
 
 
 
